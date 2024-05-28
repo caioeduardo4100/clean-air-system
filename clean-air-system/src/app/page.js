68fdc831df8851a130aa6625e25f49b5b14ddd5b
Caio Hisamatsu <134729130+caioeduardo4100@users.noreply.github.com>
2024-05-28 19:00:00 -0300

    First Commit

---
+++ clean-air-system/src/app/page.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Head from 'next/head';
+import styles from './styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Clean Air System</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}> <img src="/images/logo.png" alt="Logo" className={styles.logo} />
+        <p>Clean Air System</p> <img src="/images/logo.png" alt="second-logo" className={styles.secondLogo}></img>
+        </h1>
+
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <div className={styles.qualiContainer}>
+              <img src="/images/quality.png" alt="Qualidade do Ar Logo" className={styles.qualiLogo} />
+              <h2>Qualidade do Ar</h2>
+            </div>
+            {/* Incluir dados da qualidade do ar aqui /}
+            <p>Dados relacionados à qualidade do ar aqui...</p>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.co2Container}>
+              <img src="/images/logoCo2.png" alt="CO2 Logo" className={styles.co2Logo} />
+              <h2>CO2</h2>
+            </div>
+            {/ Incluir dados do CO2 aqui */}
+            <p>Dados relacionados ao CO2 aqui...</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
